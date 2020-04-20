d3020e66993a4be97a2e326d95e38d428724b272
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-04-19 22:20:14 -0400

    created index, knex, db-config, and migration

---
+++ data/migrations/20200419215647_create_table.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable("projects", table =>{
+      table.increments();
+      table.text("name", 128).unique().notNullable();
+      table.text("description", 255);
+      table.boolean("completed").notNullable().defaultTo(false);
+
+  })
+  .createTable("resource", table =>{
+      table.increments();
+      table.text("name").notNullable();
+      table.text("description")
+
+  })
+  .createTable("task", table =>{
+      table.increments();
+      table.text("description").notNullable();
+      table.text("notes");
+      table.boolean("completed").notNullable().defaultTo(false);
+  })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists("resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("projects");
+};
