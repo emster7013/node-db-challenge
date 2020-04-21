5af304cab6b6493e072f4c692984583d26a71aad
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-04-20 23:40:50 -0400

    deleted database readded it and still not working properly

--- data/migrations/20200419215647_create_table.js
+++ data/migrations/20200419215647_create_table.js
@@ -31,7 +31,7 @@ exports.up = function(knex) {
 exports.down = function(knex) {
     return knex.schema
     .dropTableIfExists("projects")
-    .dropTableIfExists("resources")
-    .dropTableIfExists("tasks")
+    .dropTableIfExists("resource")
+    .dropTableIfExists("task")
     
 };
--- data/seeds/03-task.js
+++ data/seeds/03-task.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("task").truncate()
-    .then(function () {
+  // return knex("task").truncate()
+  //   .then(function () {
       // Inserts seed entries
       return knex("task").insert([
         {description: 'Get someone to contact greta', notes: 'We have secured Greta for an interview'},
         {description: 'Can you smell that?', notes: 'It is harder than we though to find someone willing to trial new deoderants' },
         {description: 'Get John off his high horse and celebrate this memerable time', notes: 'He was hard to get but he finally agreed'}
       ]);
-    });
+    // });
 };
